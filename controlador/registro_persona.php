289afa900ec24be3904cc46b0d3ac57037f6ade7
Danizetaeme <danielzmansilla@gmail.com>
2023-09-20 19:27:47 +0200

    controlador basic

--- controlador/registro_persona.php
+++ controlador/registro_persona.php
@@ -16,16 +16,55 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $salary = $_POST['salary'];
 
     // Query SQL para insertar un nuevo empleado
-    $sql = "INSERT INTO employees (first_name, last_name, birth_date, gender, dept_no, title, salary) 
-            VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO employees (first_name, last_name, birth_date, gender) 
+            VALUES (?, ?, ?, ?)";
 
     // Preparar la consulta
     if ($stmt = $conexion->prepare($sql)) {
         // Vincular los parámetros
-        $stmt->bind_param("sssssss", $first_name, $last_name, $birth_date, $gender, $dept_no, $title, $salary);
+        $stmt->bind_param("ssss", $first_name, $last_name, $birth_date, $gender);
 
         // Ejecutar la consulta
         if ($stmt->execute()) {
+            // Obtener el ID del empleado recién insertado
+            $employee_id = $stmt->insert_id;
+
+            // Insertar también en la tabla de salarios (supongo que hay una relación entre employees y salaries)
+            $sql_salaries = "INSERT INTO salaries (emp_no, salary, from_date, to_date) 
+                             VALUES (?, ?, NOW(), NOW())";
+
+            if ($stmt_salaries = $conexion->prepare($sql_salaries)) {
+                // Vincular los parámetros para la tabla de salarios
+                $stmt_salaries->bind_param("id", $employee_id, $salary);
+
+                // Ejecutar la consulta para la tabla de salarios
+                $stmt_salaries->execute();
+            }
+
+            // Insertar el cargo en la tabla de títulos (supongo que hay una relación entre employees y titles)
+            $sql_titles = "INSERT INTO titles (emp_no, title, from_date, to_date) 
+                           VALUES (?, ?, NOW(), NOW())";
+
+            if ($stmt_titles = $conexion->prepare($sql_titles)) {
+                // Vincular los parámetros para la tabla de títulos
+                $stmt_titles->bind_param("is", $employee_id, $title);
+
+                // Ejecutar la consulta para la tabla de títulos
+                $stmt_titles->execute();
+            }
+
+            // Finalmente, insertar en la tabla dept_emp (supongo que hay una relación entre employees y dept_emp)
+            $sql_dept_emp = "INSERT INTO dept_emp (emp_no, dept_no, from_date, to_date) 
+                             VALUES (?, ?, NOW(), NOW())";
+
+            if ($stmt_dept_emp = $conexion->prepare($sql_dept_emp)) {
+                // Vincular los parámetros para la tabla dept_emp
+                $stmt_dept_emp->bind_param("is", $employee_id, $dept_no);
+
+                // Ejecutar la consulta para la tabla dept_emp
+                $stmt_dept_emp->execute();
+            }
+
             $response['success'] = true;
             $response['message'] = "Empleado registrado correctamente.";
         } else {
@@ -33,8 +72,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $response['message'] = "Error al registrar el empleado: " . $stmt->error;
         }
 
-        // Cerrar la consulta
+        // Cerrar las consultas
         $stmt->close();
+        if (isset($stmt_salaries)) $stmt_salaries->close();
+        if (isset($stmt_titles)) $stmt_titles->close();
+        if (isset($stmt_dept_emp)) $stmt_dept_emp->close();
     } else {
         $response['success'] = false;
         $response['message'] = "Error en la preparación de la consulta: " . $conexion->error;
